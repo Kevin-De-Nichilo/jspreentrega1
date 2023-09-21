cce0552cdadaf66f7d8eaf377ce0629fa8a185c0
Kevin De Nichilo <kevin.palomino2002@gmail.com>
2023-09-21 11:52:01 -0300

    funciones

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ class Item{
 }
 
 // Items del game.
-const pocion = new Item("Pocion de vida", 80, "pocion.png");
+const pocion = new Item("Pocion de vida", 100, "pocion.png");
 const espada = new Item("Espada", 180, "espada.png");
 const escudo = new Item("Escudo", 90, "escudo.png");
 
@@ -26,3 +26,15 @@ const Inventario = []
 
 //Oro del juego
 let oro = 500;
+
+// Funcion de comprar.
+function comprar(itemDelJuego){
+    if (oro - itemDelJuego >= 0) {
+        inventario.push(itemDelJuego);
+        oro -= itemDelJuego.precio;
+        console.log("Inventario:", Inventario);
+        console.log("Oro:", oro);
+    }else{
+        alert(`No tenes el oro suficiente para comprar ${itemDelJuego.nombre}.`);
+    }
+}
